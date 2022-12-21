50689e35dd76fe4b7bb1d5488c54fd6a28cc6e8e
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-21 11:28:39 +0530

    c

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -20,66 +20,67 @@ class ProfileScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SizedBox(
-            height: height * 0.020,
-          ),
-          Center(
-            child: CircleAvatar(
-              radius: width * 0.20,
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              foregroundImage: const NetworkImage(
-                  'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: height * 0.020,
             ),
-          ),
-          SizedBox(
-            height: height * 0.010,
-          ),
-          Text(
-            'Puneet Chauhan',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: height * 0.030,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Container(
-              height: height * 0.060,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.shopping_basket_rounded,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: width * 0.040,
-                  ),
-                  Text(
-                    'Blessed 12 people so far',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: height * 0.020,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(
-                    height: height * 0.010,
-                  ),
-                ],
+            Center(
+              child: CircleAvatar(
+                radius: width * 0.20,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundImage: const NetworkImage(
+                    'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
               ),
             ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              //physics: const NeverScrollableScrollPhysics(),
+            SizedBox(
+              height: height * 0.010,
+            ),
+            Text(
+              'Puneet Chauhan',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: height * 0.030,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                height: height * 0.060,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.shopping_basket_rounded,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: width * 0.040,
+                    ),
+                    Text(
+                      'Blessed 12 people so far',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: height * 0.020,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: height * 0.010,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
               ),
@@ -88,8 +89,8 @@ class ProfileScreen extends StatelessWidget {
                 return const Card();
               },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
