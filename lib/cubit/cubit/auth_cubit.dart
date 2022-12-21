7dc04d18df4654fe0a7d73617f1799999386caf0
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-21 12:22:40 +0530

    auth cubit

--- lib/cubit/cubit/auth_cubit.dart
+++ lib/cubit/cubit/auth_cubit.dart
@@ -1,8 +1,69 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  AuthCubit() : super(AuthInitialState()) {
+    User? currentUser = _auth.currentUser;
+    if (currentUser != null) {
+      emit(AuthLoggedInState(currentUser));
+    } else {
+      emit(AuthLoggedOutState());
+    }
+  }
+  String? _verificationId;
+
+  void sendOTP(String phoneNumber) async {
+    emit(AuthLoadingState());
+    await _auth.verifyPhoneNumber(
+      verificationCompleted: (phoneAuthCredential) {
+        signInWithPhone(phoneAuthCredential);
+      },
+      verificationFailed: (error) {
+        emit(
+          AuthErrorState(
+            error.message.toString(),
+          ),
+        );
+      },
+      codeSent: (verificationId, forceResendingToken) {
+        _verificationId = verificationId;
+        emit(
+          AuthCodeSentState(),
+        );
+      },
+      codeAutoRetrievalTimeout: (verificationId) {
+        _verificationId = verificationId;
+      },
+    );
+  }
+
+  void verifyOTP(String otp) {
+    emit(AuthLoadingState());
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: _verificationId!,
+      smsCode: otp,
+    );
+    signInWithPhone(credential);
+  }
+
+  void signInWithPhone(PhoneAuthCredential credential) async {
+    try {
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      if (userCredential.user != null) {
+        emit(AuthLoggedInState(userCredential.user!));
+      }
+    } on FirebaseAuthException catch (ex) {
+      emit(AuthErrorState(ex.message.toString()));
+    }
+  }
+
+  void logOut() async {
+    await _auth.signOut();
+    emit(AuthLoggedOutState());
+  }
 }
--- lib/cubit/cubit/auth_state.dart
+++ lib/cubit/cubit/auth_state.dart
@@ -7,4 +7,22 @@ abstract class AuthState extends Equatable {
   List<Object> get props => [];
 }
 
-class AuthInitial extends AuthState {}
+class AuthInitialState extends AuthState {}
+
+class AuthLoadingState extends AuthState {}
+
+class AuthCodeSentState extends AuthState {}
+
+class AuthCodeVerifiedState extends AuthState {}
+
+class AuthLoggedInState extends AuthState {
+  final User firebaseUser;
+  const AuthLoggedInState(this.firebaseUser);
+}
+
+class AuthLoggedOutState extends AuthState {}
+
+class AuthErrorState extends AuthState {
+  final String error;
+ const AuthErrorState(this.error);
+}
