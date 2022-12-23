52049cbd6e75f76d0e4f1acb64f1226efd861fe2
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-23 18:54:43 +0530

    service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,43 @@
 import 'package:dio/dio.dart';
+import 'package:surplus/utlis/constants.dart';
 
 class AuthService {
-  AuthService({required this.dio});
+  AuthService({required this.dio}) {
+    print('$url url');
+    dio.options.baseUrl = url;
+  }
   final Dio dio;
+
+  final baseUrl = 'http://localhost:3000';
+
+  Future sendOTP(String phone) async {
+    try {
+      final response = await dio.post(sendOtp, data: {
+        'phone': phone,
+      });
+      print(response.data + response.statusCode);
+      if (response.statusCode == 200) {
+        print('Sucess');
+        return response.data;
+      } else {
+        print('Failure');
+        print(response.statusCode);
+        return;
+      }
+    } catch (error) {
+      print(error);
+    } on DioError catch (error) {
+      print(error.message);
+    }
+  }
+
+  Future verifyOTP(String phone, String otp) async {
+    try {
+      final response = await dio.post(validateOTP, data: {
+        'phone': phone,
+        'otp': otp,
+      });
+      if (response.statusCode == 200) {}
+    } catch (error) {}
+  }
 }
---
+++ lib/utlis/constants.dart
@@ -0,0 +1,13 @@
+
+
+const httpScheme = 'http';
+const baseUrl = 'localhost:3000';
+
+const url = '$httpScheme://$baseUrl';
+
+
+const registerUser = '/register';
+const sendOtp = '/sendOTP';
+const validateOTP = '/validateOTP';
+const getAllUsers = '/allUsers';
+
