5dcdfaa6696977924caae6f07d9b685d7c17877c
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-12-31 11:58:35 +0530

    c

--- lib/utlis/constants.dart
+++ lib/utlis/constants.dart
@@ -1,9 +1,11 @@
 
 
 const httpScheme = 'http';
-const baseUrl = '192.168.43.179:3000';
+const baseUrl = '192.168.0.103:3000';
+const apiPath = '/api';
+const apiVersion = '/vi';
 
-const url = '$httpScheme://$baseUrl';
+const url = '$httpScheme://$baseUrl$apiPath$apiVersion';
 
 
 const registerUser = '/register';
