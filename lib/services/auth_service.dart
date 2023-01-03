29301f3f2b304cb18ade15988c0cea5fa4bb68ca
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2023-01-03 10:01:37 +0530

    changes

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -17,7 +17,7 @@ class AuthService {
   Future<JsonResponse> sendOTP(String phone) async {
     try {
       final response = await dio
-          .post('http://192.168.43.179:3000/api/v1/users/sendOTP', data: {
+          .post('http://192.168.0.106:3000/api/v1/users/sendOTP', data: {
         'phone': phone,
       });
       // print(response.data + response.statusCode);
@@ -46,7 +46,7 @@ class AuthService {
   Future<JsonResponse> verifyOTP(String phone, String otp) async {
     try {
       final response = await dio
-          .post('http://192.168.43.179:3000/api/v1/users/validateOTP', data: {
+          .post('http://192.168.0.106:3000/api/v1/users/validateOTP', data: {
         'phone': phone,
         'otp': otp,
       });
@@ -76,7 +76,7 @@ class AuthService {
     try {
       final formData = FormData.fromMap(user.toJson());
       print('fiels: ${formData.files} ${formData.fields}');
-      final response = await dio.post(registerUser, data: formData);
+      final response = await dio.post('http://192.168.0.106:3000/api/v1/users//register', data: formData);
       if (response.statusCode == 200) {
         //final user = User.fromJson(response.data)
         return JsonResponse.success(
